4192faae47d8caa7aeab85b2afc08dac0c2aa0c8
JenniferFromanger <jennifer.mirre@yahoo.fr>
2020-03-26 11:56:58 +0100

    finish homepage

--- header.js
+++ header.js
@@ -1,3 +1,4 @@
+/*-------Burger Menu-------*/
 const burgerButton = document.querySelector('#hamburger');
 const sideBar = document.querySelector('#deroulantBurger');
 function clickLink() {
@@ -5,3 +6,12 @@ function clickLink() {
 };
 
 burgerButton.addEventListener('click', clickLink);
+
+const closeMenu = document.querySelectorAll('a.closeClic');
+
+for (let i = 0; i < closeMenu.length; i++) {
+    closeMenu[i].addEventListener('click', clickLink);
+};
+
+
+/*-------Main pictures-----*/
